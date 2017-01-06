15ba7693052252ae0601b96e033ee052ab4074a5
Dave Pagurek <dave@davepagurek.com>
2017-01-06 08:39:33 -0500

    Change deploy ignores

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ message="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
 
 rm .gitignore # We want all compiled assets! ALL OF THEM
+mv .gitignore.deploy .gitignore
 git add --all
 git commit -m "$message"
 git push --force --quiet "https://github.com/davepagurek/SE-Notes.git" master:website
